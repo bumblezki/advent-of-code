c0c7e557cc9b7237d49a57093d9c362fcebfbbac
Alexander Tometzki <alex.tometzki@icloud.com>
2022-02-26 18:58:58 +0000

    Solve day 3 part 2

--- year18/src/day03.rs
+++ year18/src/day03.rs
@@ -1,57 +1,108 @@
 // Potential improvements:
 //
 use std::collections::HashSet;
+use itertools::Itertools;
 use regex::Regex;
 use std::str::FromStr;
 
-#[derive(Debug)]
-struct FabricClaim {
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+struct SquareInch {
     x: i32,
-    y: i32,
+    y: i32
+}
+
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct FabricClaim {
+    id: i32,
+    top_left: SquareInch,
+    top_right: SquareInch,
+    bottom_left: SquareInch,
     width: i32,
     height: i32,
 }
 
 impl FabricClaim {
-    fn into_coords(&self) -> Vec<(i32, i32)> {
-        let mut coords = Vec::new();
-        for i in self.x..self.x+self.width {
-            for j in self.y..self.y+self.height {
-                coords.push( (i, j) );
+    fn new(id: i32, x: i32, y: i32, width: i32, height: i32) -> Self {
+        Self {
+            id,
+            top_left: SquareInch { x, y },
+            top_right: SquareInch { x: x+width, y },
+            bottom_left: SquareInch { x, y: y+height },
+            width,
+            height,
+        }
+    }
+
+    fn all_sq_inches(&self) -> Vec<SquareInch> {
+        let mut sq_inches = Vec::with_capacity(
+            self.height as usize * self.width as usize
+        );
+        for x in self.top_left.x..self.top_right.x {
+            for y in self.top_left.y..self.bottom_left.y {
+                sq_inches.push( SquareInch { x, y } );
             }
         }
-        coords
+        sq_inches
     }
 
     fn from_input_line(input_line: &str) -> FabricClaim {
-        let re = Regex::new(r"(\d+),(\d+): (\d+)x(\d+)").unwrap();
-        re.captures(&input_line).map(|cap| {
-            FabricClaim {
-                x: FromStr::from_str(&cap[1]).unwrap(),
-                y: FromStr::from_str(&cap[2]).unwrap(),
-                width: FromStr::from_str(&cap[3]).unwrap(),
-                height: FromStr::from_str(&cap[4]).unwrap(),
-            }
+        let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+        re.captures(input_line).map(|cap| {
+            let id = FromStr::from_str(&cap[1]).unwrap();
+            let x = FromStr::from_str(&cap[2]).unwrap();
+            let y = FromStr::from_str(&cap[3]).unwrap();
+            let width = FromStr::from_str(&cap[4]).unwrap();
+            let height = FromStr::from_str(&cap[5]).unwrap();
+            FabricClaim::new(id, x, y, width, height)
         }).unwrap()
+    }
+
+    fn overlaps(&self, other: &FabricClaim) -> bool {
+        if self.top_left.x > other.top_right.x ||
+        self.top_right.x < other.top_left.x ||
+        self.top_left.y > other.bottom_left.y ||
+        self.bottom_left.y < other.top_left.y {
+            false
+        } else {
+            true
+        }
     }
 }
 
 pub fn day03(input_lines: &[Vec<String>]) -> (String, String) {
 
-    let (claimed_inches, contested_inches) = input_lines[0].iter().map(|line| {
-        FabricClaim::from_input_line(line)
-    }).fold((HashSet::<(i32, i32)>::new(), HashSet::<(i32, i32)>::new()), |(mut claimed, mut contested), claim| {
-        let square_inches = claim.into_coords();
-        for square_inch in square_inches {
-            if !claimed.insert(square_inch) {
-                contested.insert(square_inch);
-            }
-        }
-        (claimed, contested)
+    let fabric_claims: HashSet<FabricClaim> = input_lines[0].iter().fold(HashSet::new(), |mut map, line| {
+        let fabric_claim = FabricClaim::from_input_line(line);
+        map.insert(fabric_claim);
+        map
     });
+
+    let (_, contested_inches) = fabric_claims.iter()
+        .fold((HashSet::<SquareInch>::new(), HashSet::<SquareInch>::new()), |(mut claimed, mut contested), claim| {
+            let square_inches = claim.all_sq_inches();
+            for square_inch in square_inches {
+                if !claimed.insert(square_inch) {
+                    contested.insert(square_inch);
+                }
+            }
+            (claimed, contested)
+        }
+    );
+
+    let all_claim_ids: HashSet<i32> = HashSet::from_iter(1..fabric_claims.len() as i32 + 1);
+    let mut contested_claim_ids = HashSet::<i32>::new();
+    for combination in fabric_claims.iter().combinations(2) {
+        let this_claim = combination[0];
+        let that_claim = combination[1];
+        if this_claim.overlaps(that_claim) {
+            contested_claim_ids.insert(this_claim.id);
+            contested_claim_ids.insert(that_claim.id);
+        }
+    }
+
     let answer1 = contested_inches.len();
-    let answer2 = 0;
-    (format!("{:?}", answer1), format!("{:?}", answer2))
+    let answer2 = all_claim_ids.difference(&contested_claim_ids); // all_ids.difference(&contested_ids);
+    (format!("{}", answer1), format!("{:?}", answer2))
 }
 
 #[cfg(test)]
