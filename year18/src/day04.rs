a3ff7ed1b6ea9e07c00f09d6c46fed791e2198cd
Alexander Tometzki <alex.tometzki@icloud.com>
2022-03-01 21:03:53 +0000

    Create enum for SecurityEventType

--- year18/src/day04.rs
+++ year18/src/day04.rs
@@ -1,27 +1,42 @@
 use std::cmp::Ordering;
 use std::iter::FromIterator;
-
-// Potential improvements:
-//
+use std::str::FromStr;
 use chrono::prelude::*;
 use dateparser::parse;
 use regex::Regex;
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum SecurityEventType {
+    Wake,
+    Sleep,
+    StartShift(i32)
+}
+
 #[derive(Debug, Eq, PartialEq, PartialOrd)]
 struct SecurityEvent {
     time: DateTime<Utc>,
-    description: String
+    event_type: SecurityEventType
 }
 
 impl SecurityEvent {
     fn new(time: DateTime<Utc>, description: &str) -> Self {
-        Self {
-            time,
-            description: description.to_string()
-        }
+        let event_type = match description {
+            "wakes up" => SecurityEventType::Wake,
+            "falls asleep" => SecurityEventType::Sleep,
+            description => {
+                let re = Regex::new(r"Guard #(\d+) begins shift").unwrap();
+                re.captures(description).map(|cap| {
+                    let id = FromStr::from_str(&cap[1]).unwrap();
+                    SecurityEventType::StartShift(id)
+                }).unwrap()
+            }
+        };
+        Self { time, event_type }
     }   
 
     fn from_input_line(input_line: &String) -> Self {
+        // We can't parse dates before 1970 into a DateTime object.
+        // So manipulate the strings to effectively add 1000 years to all each date.
         let mut input_line_chars: Vec<char> = input_line.clone().chars().collect();
         input_line_chars[1] = '2';
         let input_line_plus_1000y = String::from_iter(input_line_chars);
