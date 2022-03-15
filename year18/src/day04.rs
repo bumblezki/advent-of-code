a8733f0b8daccfd9b7a52451e9e595e5860b0f4b
Alexander Tometzki <alex.tometzki@icloud.com>
2022-03-15 13:30:37 -0700

    Clean up day 4 code.

--- year18/src/day04.rs
+++ year18/src/day04.rs
@@ -29,16 +29,24 @@ impl SecurityEvent {
 
     fn from_input_line(input_line: &String) -> Self {
         // We can't parse dates before 1970 into a DateTime object.
+        // The actual year doesn't matter, only the order and the minute matter.
         // So manipulate the strings to effectively add 1000 years to all each date.
         let mut input_line_chars: Vec<char> = input_line.clone().chars().collect();
         input_line_chars[1] = '2';
         let input_line_plus_1000y = String::from_iter(input_line_chars);
         
+        // Split the line on either "[" or "] ".
+        // E.g., "[1518-10-09 00:56] wakes up" goes to ['', '2518-10-09 00:56', 'wakes up'].
         let re = Regex::new(r"\[|\] ").unwrap();
         let split_line: Vec<&str> = re.split(&input_line_plus_1000y).collect();
+        let date_str = split_line[1];
+        let event_str = split_line[2];
+
+        // Parse the str type date into a DateTime object.
+        let time = dateparser::parse(date_str).unwrap();
         
-        let time = dateparser::parse(split_line[1]).unwrap();
-        let event_type = match split_line[2] {
+        // Parse the str type event into a SecurityEventType object.
+        let event_type = match event_str {
             "wakes up" => SecurityEventType::Wake,
             "falls asleep" => SecurityEventType::Sleep,
             description => {
@@ -46,7 +54,7 @@ impl SecurityEvent {
                 re.captures(description).map(|cap| {
                     let id = FromStr::from_str(&cap[1]).unwrap();
                     SecurityEventType::StartShift(id)
-                }).unwrap()
+                }).expect("Failed to capture regular expression from security event.")
             }
         };
         SecurityEvent::new(time, event_type)
@@ -65,20 +73,18 @@ impl Ord for SecurityEvent {
     }
 }
 
-fn count_minutes_asleep(events: &Vec<&SecurityEvent>) -> u32 {
-    events.iter().filter(|event| event.event_type == SecurityEventType::Sleep).zip(
-        events.iter().filter(|event| event.event_type == SecurityEventType::Wake)
+fn count_minutes_asleep(events: &Vec<SecurityEvent>) -> u32 {
+    events.iter().filter(|&event| event.event_type == SecurityEventType::Sleep).zip(
+        events.iter().filter(|&event| event.event_type == SecurityEventType::Wake)
     ).fold(0, |mut total, (sleep, wake)| {
-        for _ in sleep.minute..wake.minute {
-            total += 1;
-        }
+        total += wake.minute - sleep.minute;
         total
     })
 }
 
-fn map_minutes_asleep(events: &Vec<&SecurityEvent>) -> Counter<u32> {
-    events.iter().filter(|event| event.event_type == SecurityEventType::Sleep).zip(
-        events.iter().filter(|event| event.event_type == SecurityEventType::Wake)
+fn map_minutes_asleep(events: &Vec<SecurityEvent>) -> Counter<u32> {
+    events.iter().filter(|&event| event.event_type == SecurityEventType::Sleep).zip(
+        events.iter().filter(|&event| event.event_type == SecurityEventType::Wake)
     ).fold(Counter::new(), |mut counter, (sleep, wake)| {
         counter.extend((sleep.minute..wake.minute).collect::<Counter<_>>());
         counter
@@ -88,74 +94,72 @@ fn map_minutes_asleep(events: &Vec<&SecurityEvent>) -> Counter<u32> {
 pub fn day04(input_lines: &[Vec<String>]) -> (String, String) {
 
     // Parse the input into a vector of all security events and sort these chronologically.
-    let mut all_security_events: Vec<SecurityEvent> = input_lines[0].iter().map(|line | {
-        SecurityEvent::from_input_line(line)
-    }).collect();
+    let mut all_security_events: Vec<SecurityEvent> = input_lines[0]
+        .iter()
+        .map(|line | {
+                SecurityEvent::from_input_line(line)
+            }
+        ).collect();
     all_security_events.sort_by(|event1, event2| event1.cmp(event2));
 
-    // Convert the vector of all security events into a HashMap where the key is the ID of a guard and the
-    // value is a vector of all the security events associated with that guard that are of type Wake or Sleep.
+    // Convert the vector of all security events into a HashMap where the key is the ID of a
+    // guard and the value is a vector of all the Wake and Sleep security events associated
+    // with that guard.
     let mut guard_id: u32 = 0;
-    let security_events_per_guard: HashMap<u32, Vec<&SecurityEvent>> = all_security_events
-        .iter().fold(HashMap::new(), |mut hashmap, event| {
-            if let SecurityEventType::StartShift(id) = event.event_type {
-                guard_id = id;
-                if !hashmap.contains_key(&guard_id) {
-                    hashmap.insert(guard_id, Vec::new());
-                }
-            } else {
-                hashmap.entry(guard_id).and_modify(|events| events.push(event));
+    let mut security_events_per_guard = HashMap::<u32, Vec<SecurityEvent>>::new();
+    for security_event in all_security_events {
+        if let SecurityEventType::StartShift(id) = security_event.event_type {
+            guard_id = id;
+            if !security_events_per_guard.contains_key(&guard_id) {
+                security_events_per_guard.insert(guard_id, Vec::new());
             }
-            hashmap
+        } else {
+            security_events_per_guard
+                .entry(guard_id)
+                .and_modify(|events| events.push(security_event));
         }
-    );
-    // A HashMap where the key is the ID a guard and the value is a the total time they spent asleep while on shift.
-    let total_minutes_asleep_per_guard = security_events_per_guard
-        .iter().fold(HashMap::new(), |mut map, (&guard_id, events)| {
-            map.insert(guard_id, count_minutes_asleep(events));
-            map
-        }
-    );
-    // A HashMap where the key is the ID a guard and the value is a vector of all the security events associated with
-    // that guard in chronological order.
-    let sleep_schedules_per_guard = &security_events_per_guard
-        .iter().fold(HashMap::new(), |mut map, (&guard_id, events)| {
-            map.insert(guard_id, map_minutes_asleep(events));
-            map
-        }
-    );
+    }
+
+    // A HashMap where the key is the ID a guard and the value is a the total time they spent
+    // asleep while on shift.
+    let mut total_minutes_asleep_per_guard = HashMap::<u32, u32>::new();
+    // A HashMap where the key is the ID a guard and the value is a vector of all the security
+    // events associated with that guard in chronological order.
+    let mut sleep_schedules_per_guard = HashMap::<u32, Counter<u32>>::new();
+    for (guard_id, security_events) in security_events_per_guard {
+        total_minutes_asleep_per_guard.insert(guard_id, count_minutes_asleep(&security_events));
+        sleep_schedules_per_guard.insert(guard_id, map_minutes_asleep(&security_events));
+    }
 
     // Part 1
     let id_of_guard_with_most_total_minutes_asleep = total_minutes_asleep_per_guard
         .iter()
-        .max_by(|&guard1_id_and_total, &guard2_id_and_total| guard1_id_and_total.1.cmp(&guard2_id_and_total.1))
-        .map(|(id, _total)| id )
-        .unwrap();
-    let sleep_schedule_of_guard_with_most_total_minutes_asleep = sleep_schedules_per_guard.get(id_of_guard_with_most_total_minutes_asleep).unwrap();
+        .max_by(|&(_this_guard_id, &this_guard_total), &(&_that_guard_id, &that_guard_total)| this_guard_total.cmp(&that_guard_total))
+        .map(|(&guard_id, _guard_total)| guard_id )
+        .expect("Failed to find the ID of the guard who spent the most time asleep.");
+    let sleep_schedule_of_guard_with_most_total_minutes_asleep = sleep_schedules_per_guard.get(&id_of_guard_with_most_total_minutes_asleep).unwrap();
     let sleepiest_minute_of_guard_with_most_total_minutes_asleep = sleep_schedule_of_guard_with_most_total_minutes_asleep
         .iter()
-        .max_by(|&minute1_and_total, &minute2_and_total| minute1_and_total.1.cmp(&minute2_and_total.1))
-        .map(|(minute, _total)| minute)
-        .unwrap();
+        .max_by(|&(_this_minute, &this_total), &(&_that_minute, &that_total)| this_total.cmp(&that_total))
+        .map(|(&minute, _total)| minute)
+        .expect("Failed to find the ID of the guard who was asleep on the same minute the most.");
     let answer1 = id_of_guard_with_most_total_minutes_asleep * sleepiest_minute_of_guard_with_most_total_minutes_asleep;
 
     // Part 2
-    let most_consistently_asleep_minute_and_count_per_guard = sleep_schedules_per_guard
-        .iter()
-        .fold(HashMap::new(), |mut map, (&guard_id, sleep_map)| {
-            if let Some(most_consistently_asleep_minute_and_count) = sleep_map
+    let mut most_consistently_asleep_minute_and_count_per_guard = HashMap::<u32, (u32, usize)>::new();
+    for (guard_id, sleep_schedule) in sleep_schedules_per_guard {
+        if let Some(most_consistently_asleep_minute_and_count) = sleep_schedule
                 .iter()
-                .max_by(|&a, &b| a.1.cmp(&b.1))
-                .map(|(minute, count)| (minute, count)) {
-                map.insert(guard_id, most_consistently_asleep_minute_and_count);
-            }
-            map
+                .max_by(|&(_this_minute, &this_count), &(_that_minute, &that_count)| this_count.cmp(&that_count))
+                .map(|(&minute, &count)| (minute, count)) {
+            most_consistently_asleep_minute_and_count_per_guard.insert(guard_id, most_consistently_asleep_minute_and_count);
         }
-    );
+    }
+    
     let answer2 = most_consistently_asleep_minute_and_count_per_guard
         .iter()
-        .max_by(|&(_, &guard1_minute_and_count), &(_, &guard2_minute_and_count)| guard1_minute_and_count.1.cmp(&guard2_minute_and_count.1))
-        .map(|(id, &(minute, _count))| id * minute)
+        .max_by(|&(_this_guard_id, &(_this_guard_minute, this_guard_count)), &(_that_guard_id, &(_that_guard_minute, that_guard_count))| this_guard_count.cmp(&that_guard_count))
+        .map(|(&guard_id, &(minute, _count))| guard_id * minute)
         .unwrap();
 
     (format!("{}", answer1), format!("{}", answer2))
