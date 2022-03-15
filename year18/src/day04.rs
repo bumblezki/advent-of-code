2460c2295b62af3cf72f257b86c1cca04f28873e
Alexander Tometzki <alex.tometzki@icloud.com>
2022-03-15 11:46:16 -0700

    Add test input for day 4

--- year18/src/day04.rs
+++ year18/src/day04.rs
@@ -169,9 +169,25 @@ mod tests {
     #[test]
     fn check_day04_case01() {
         full_test(
-"", // INPUT STRING
-"0", // PART 1 RESULT
-"0" // PART 2 RESULT
+"[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up", // INPUT STRING
+"240", // PART 1 RESULT
+"4455" // PART 2 RESULT
         )
     }
 
