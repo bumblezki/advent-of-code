60ff2f86677a109dd15a9e86520e614b7601fdb2
Alexander Tometzki <alex.tometzki@icloud.com>
2022-03-17 16:10:04 -0700

    Solve day 5

--- year18/src/day05.rs
+++ year18/src/day05.rs
@@ -1,19 +1,94 @@
-use itertools::Itertools;
+use std::fmt;
 
-pub fn day05(input_lines: &[Vec<String>]) -> (String, String) {
-    
-    let mut original_chain = input_lines[0][0].chars();
-    loop {
-        let (current, next) = match original_chain.next_tuple() {
-            Some((char1, char2)) => (char1, char2),
-            None => break
+const ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
+
+#[derive(Clone, Debug, PartialEq)]
+enum Polarity {
+    Upper,
+    Lower
+}
+
+#[derive(Clone, Debug)]
+struct PolymerUnit {
+    raw_char: char,
+    lowercase_char: char,
+    polarity: Polarity,
+}
+
+impl PolymerUnit {
+
+    fn new(char: char) -> Self {
+        let polarity: Polarity = if char.is_lowercase() {
+            Polarity::Lower
+        } else {
+            Polarity::Upper
         };
-        if current == next {
-            break
+        Self {
+            raw_char: char,
+            lowercase_char: char.to_ascii_lowercase(),
+            polarity,
         }
     }
-    let answer1 = 0;
-    let answer2 = 0;
+
+    fn reacts_with(&self, other: &Self) -> bool {
+        self.lowercase_char == other.lowercase_char && self.polarity != other.polarity
+    }
+
+    fn is_type(&self, char: char) -> bool {
+        self.lowercase_char == char.to_ascii_lowercase()
+    }
+}
+
+impl fmt::Display for PolymerUnit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.raw_char)
+    }
+}
+
+fn react_polymer_chain(chain: &mut Vec<PolymerUnit>) -> &mut Vec<PolymerUnit> {
+    let mut restart_idx: usize = 0;
+    'outer: loop {
+        for (idx, pair) in chain.windows(2).enumerate().skip(restart_idx) {
+            if pair[0].reacts_with(&pair[1]) {
+                chain.remove(idx);
+                chain.remove(idx);
+                if idx != 0 {
+                    restart_idx = idx - 1;
+                } else {
+                    restart_idx = 0;
+                }
+                continue 'outer;
+            }
+        }
+        return chain;
+    }
+    
+}
+
+pub fn day05(input_lines: &[Vec<String>]) -> (String, String) {
+    let original_polymer_chain = input_lines[0][0]
+        .chars()
+        .map(|char| PolymerUnit::new(char))
+        .collect::<Vec<PolymerUnit>>();
+
+    let mut polymer_chain = original_polymer_chain.clone();
+    let reacted_polymer_chain = react_polymer_chain(&mut polymer_chain);
+    let answer1 = reacted_polymer_chain.len();
+
+    let mut shortest_reacted_polymer_chain_len = original_polymer_chain.len();
+    for char in ALPHABET.chars() {
+        let mut polymer_chain = original_polymer_chain
+            .clone()
+            .into_iter()
+            .filter(|unit| !unit.is_type(char))
+            .collect::<Vec<PolymerUnit>>();
+        let reacted_polymer_chain = react_polymer_chain(&mut polymer_chain);
+        if reacted_polymer_chain.len() < shortest_reacted_polymer_chain_len {
+            shortest_reacted_polymer_chain_len = reacted_polymer_chain.len();
+        }
+    }
+
+    let answer2 = shortest_reacted_polymer_chain_len;
     (format!("{:?}", answer1), format!("{}", answer2))
 }
 
@@ -25,9 +100,9 @@ mod tests {
     #[test]
     fn check_day05_case01() {
         full_test(
-"", // INPUT STRING
-"0", // PART 1 RESULT
-"0" // PART 2 RESULT
+"dabAcCaCBAcCcaDA", // INPUT STRING
+"10", // PART 1 RESULT
+"4" // PART 2 RESULT
         )
     }
 
