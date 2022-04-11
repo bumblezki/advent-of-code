24c03af3e10fa907d7a198bf44a3dc3977d15cd1
Alexander Tometzki <alex.tometzki@icloud.com>
2022-04-11 22:15:12 +0100

    Solve day 6 part 1

--- year18/src/day06.rs
+++ year18/src/day06.rs
@@ -1,9 +1,10 @@
 // Potential improvements:
 //
 
-use std::{str::FromStr, num::ParseIntError};
+use std::{str::FromStr, num::ParseIntError, collections::{HashMap, HashSet}};
 
-#[derive(Clone, Debug, PartialEq)]
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct Point {
     x: i32,
     y: i32
@@ -31,22 +32,21 @@ impl Point {
         return (self.x - other.x).abs() + (self.y - other.y).abs()
     }
 
-    fn get_shortest_manhattan_distance(&self, others: &Vec<Point>) -> Option<(Point, i32)> {
-        let shortest_distances: Vec<(Point, i32)> =  others
-            .clone()
+    fn get_closest_destination(&self, destinations: &Vec<Point>) -> Option<Point> {
+        let destination_distances: Vec<(Point, i32)> = destinations
             .iter()
-            .map(|other| (other.clone(), self.manhattan_distance(&other)))
+            .map(|dest| (dest.clone(), self.manhattan_distance(&dest)))
             .collect();
-        let shortest_distance: Option<(Point, i32)> = shortest_distances
+        let shortest_distance: Option<(Point, i32)> = destination_distances
             .clone()
             .into_iter()
             .min_by(|&(_, d1), &(_, d2)| d1.cmp(&d2));
         match shortest_distance {
             Some((point, d)) => {
-                if shortest_distances.iter().filter(|&(_, distance)| distance == &d).count() > 1 {
+                if destination_distances.iter().filter(|&(_, distance)| distance == &d).count() > 1 {
                     return None
                 } else {
-                    return Some((point.clone(), d))
+                    return Some(point)
                 }
             }
             None => return None
@@ -54,12 +54,62 @@ impl Point {
     }
 }
 
+fn get_max_x_and_y(destinations: &Vec<Point>) -> (i32, i32) {
+    (
+        destinations.iter().max_by(|a, b| a.x.cmp(&b.x)).expect("Failed to find max X value.").x,
+        destinations.iter().max_by(|a, b| a.y.cmp(&b.y)).expect("Failed to find max Y value.").y
+    )
+}
+
+fn get_all_points(max_x: &i32, max_y: &i32) -> Vec<Point> {
+    let mut all_points: Vec<Point> = Vec::new();
+    for x in 0..=*max_x {
+        for y in 0..=*max_y {
+            all_points.push(Point::new(x, y))
+        }
+    }
+    all_points
+}
+
+fn get_edges(max_x: &i32, max_y: &i32) -> Vec<Point> {
+    let mut edges: Vec<Point> = Vec::new();
+    for x in 0..=*max_x {
+        edges.push(Point::new(x, 0));
+        edges.push(Point::new(x, *max_y));
+    }
+    for y in 0..=*max_y {
+        edges.push(Point::new(0, y));
+        edges.push(Point::new(*max_x, y));
+    }
+    edges
+}
+
 // Modelled off https://davidburn.github.io/advent-2018/day6/
 pub fn day06(input_lines: &[Vec<String>]) -> (String, String) {
-    let points: Vec<Point> = input_lines[0].iter().map(|line| line.parse::<Point>().unwrap()).collect();
-    println!("{:?}", points);
+    let destinations: Vec<Point> = input_lines[0].iter().map(|line| line.parse::<Point>().unwrap()).collect();
+    let (max_x, max_y): (i32, i32) = get_max_x_and_y(&destinations);
+    let edges: Vec<Point> = get_edges(&max_x, &max_y);
+    let all_points: Vec<Point> = get_all_points(&max_x, &max_y);
 
-    let answer1 = 0;
+    let mut infite_destinations: HashSet<Point> = HashSet::new();
+    let mut destination_map: HashMap<Point, i32> = HashMap::new();
+    for point in all_points {
+        if let Some(closest_destination) = point.get_closest_destination(&destinations) {
+            if edges.contains(&point) {
+                infite_destinations.insert(closest_destination.clone());
+            }
+            *destination_map.entry(closest_destination).or_insert(0) += 1;
+        }
+    }
+
+    let (_, area): (&Point, &i32) = destination_map
+        .iter()
+        .filter(|&(dest, _)| !infite_destinations.contains(dest))
+        .max_by(|&(_, &a), &(_, &b)| a.cmp(&b))
+        .expect("Failed to find maximum area.");
+
+
+    let answer1 = area;
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
 }
