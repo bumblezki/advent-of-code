9ca2dd4fac8babcad5f8dda5cfaa03fb6fd2ec9b
aslt <alex.tometzki@metaswitch.com>
2022-04-11 14:10:01 +0100

    Make a start on day6

--- year18/src/day06.rs
+++ year18/src/day06.rs
@@ -1,7 +1,64 @@
 // Potential improvements:
 //
 
-pub fn day06(_input_lines: &[Vec<String>]) -> (String, String) {
+use std::{str::FromStr, num::ParseIntError};
+
+#[derive(Clone, Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
+impl FromStr for Point {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let coords: Vec<&str> = s.split(", ").collect();
+
+        let x_fromstr = coords[0].parse::<i32>()?;
+        let y_fromstr = coords[1].parse::<i32>()?;
+
+        Ok(Point { x: x_fromstr, y: y_fromstr })
+    }
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Point { x, y }
+    }
+
+    fn manhattan_distance(&self, other: &Self) -> i32 {
+        return (self.x - other.x).abs() + (self.y - other.y).abs()
+    }
+
+    fn get_shortest_manhattan_distance(&self, others: &Vec<Point>) -> Option<(Point, i32)> {
+        let shortest_distances: Vec<(Point, i32)> =  others
+            .clone()
+            .iter()
+            .map(|other| (other.clone(), self.manhattan_distance(&other)))
+            .collect();
+        let shortest_distance: Option<(Point, i32)> = shortest_distances
+            .clone()
+            .into_iter()
+            .min_by(|&(_, d1), &(_, d2)| d1.cmp(&d2));
+        match shortest_distance {
+            Some((point, d)) => {
+                if shortest_distances.iter().filter(|&(_, distance)| distance == &d).count() > 1 {
+                    return None
+                } else {
+                    return Some((point.clone(), d))
+                }
+            }
+            None => return None
+        }
+    }
+}
+
+// Modelled off https://davidburn.github.io/advent-2018/day6/
+pub fn day06(input_lines: &[Vec<String>]) -> (String, String) {
+    let points: Vec<Point> = input_lines[0].iter().map(|line| line.parse::<Point>().unwrap()).collect();
+    println!("{:?}", points);
+
     let answer1 = 0;
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
