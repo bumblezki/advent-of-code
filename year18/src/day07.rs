57388c7f1951b423de7cf308cc12b0ca6c85fa76
Alexander Tometzki <alex.tometzki@icloud.com>
2022-05-02 21:19:44 +0100

    solve day 7 part 1

--- year18/src/day07.rs
+++ year18/src/day07.rs
@@ -1,8 +1,81 @@
 // Potential improvements:
 //
 
-pub fn day07(_input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = 0;
+use std::collections::{BTreeMap, HashSet};
+use regex::Regex;
+
+struct Graph {
+    edges: BTreeMap<char, HashSet<char>>,
+}
+
+impl Graph {
+    fn from_input(input_lines: &Vec<String>) -> Self {
+        let mut edges = BTreeMap::<char, HashSet<char>>::new();
+        let mut steps = HashSet::<char>::new();
+        let re = Regex::new(r"Step | must be finished before step | can begin.").unwrap();
+        for line in input_lines {
+            let chars: Vec<char> = re.replace_all(line, "").chars().collect();
+            let u: char = chars[0];
+            let v: char = chars[1];
+            steps.insert(u);
+            steps.insert(v);
+            let requirements = edges.entry(v).or_insert(HashSet::new());
+            requirements.insert(u);
+        }
+        for step in steps {
+            if !edges.contains_key(&step) {
+                edges.insert(step, HashSet::<char>::new());
+            }
+        }
+        Graph { edges }
+    }
+
+    fn complete(&mut self, step: &char) {
+        self.edges.remove(&step);
+        for prereqs in self.edges.values_mut() {
+            prereqs.remove(&step);
+        }
+        println!("Completed: {}", step);
+    }
+}
+
+impl std::fmt::Display for Graph {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (step, prereqs) in self.edges.iter() {
+            writeln!(f, "{}:     {:?}", step, prereqs)?;
+        }
+        Ok(())
+    }
+}
+
+fn queue_completed_steps(q: &mut Vec<char>, graph: &mut Graph) {
+    for (step, prereqs) in graph.edges.iter() {
+        if !q.contains(step) && prereqs.is_empty() {
+            q.push(step.clone());
+            q.sort();
+            q.reverse();
+        }
+    }
+}
+
+pub fn day07(input_lines: &[Vec<String>]) -> (String, String) {
+    let mut graph = Graph::from_input(&input_lines[0]);
+    let mut order: Vec<char> = Vec::new();
+    let mut q: Vec<char> = Vec::new();
+    queue_completed_steps(&mut q, &mut graph);
+    loop {
+        println!("{}", graph);
+        println!("{:?}", q);
+        match q.pop() {
+            Some(step) => {
+                graph.complete(&step);
+                queue_completed_steps(&mut q, &mut graph);
+                order.push(step);
+            },
+            None => break,
+        }
+    }
+    let answer1 = String::from_iter(order);
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
 }
@@ -15,8 +88,14 @@ mod tests {
     #[test]
     fn check_day07_case01() {
         full_test(
-"", // INPUT STRING
-"0", // PART 1 RESULT
+"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.", // INPUT STRING
+"CABDFE", // PART 1 RESULT
 "0" // PART 2 RESULT
         )
     }
