e12e2ac38a205997f821b8c10c53755733d49376
Alexander Tometzki <alex.tometzki@icloud.com>
2022-06-16 00:38:36 +0100

    solve day 7 part 2

--- year18/src/day07.rs
+++ year18/src/day07.rs
@@ -1,13 +1,15 @@
 // Potential improvements:
 //
 
-use std::collections::{BTreeMap, HashSet};
+use itertools::all;
 use regex::Regex;
+use std::collections::{BTreeMap, HashSet};
 
-fn secs(c: char) -> i32 {
-    c as i32 - 4
+fn secs(task: char) -> i32 {
+    task as i32 - 4
 }
 
+#[derive(Clone)]
 struct Graph {
     edges: BTreeMap<char, HashSet<char>>,
     queue: Vec<char>,
@@ -32,7 +34,10 @@ impl Graph {
                 edges.insert(step, HashSet::<char>::new());
             }
         }
-        Graph { edges, queue: Vec::new() }
+        Graph {
+            edges,
+            queue: Vec::new(),
+        }
     }
 
     fn complete(&mut self, step: &char) {
@@ -43,9 +48,12 @@ impl Graph {
         println!("Completed: {}", step);
     }
 
-    fn queue_completed_steps(&mut self) {
-        for (step, prereqs) in self.edges.iter() {
-            if !self.queue.contains(step) && prereqs.is_empty() {
+    fn queue_completed_steps(&mut self, in_progress: &Vec<Option<char>>) {
+        for (&step, prereqs) in self.edges.iter() {
+            if !self.queue.contains(&step)
+                && prereqs.is_empty()
+                && !in_progress.contains(&Some(step))
+            {
                 self.queue.push(step.clone());
                 self.queue.sort();
                 self.queue.reverse();
@@ -65,22 +73,63 @@ impl std::fmt::Display for Graph {
 }
 
 pub fn day07(input_lines: &[Vec<String>]) -> (String, String) {
-    let mut graph = Graph::from_input(&input_lines[0]);
-    let mut order: Vec<char> = Vec::new();
-    graph.queue_completed_steps();
+    let graph = Graph::from_input(&input_lines[0]);
+    let mut order1: Vec<char> = Vec::new();
+    let mut graph1 = graph.clone();
+    graph1.queue_completed_steps(&vec![]);
     loop {
         // println!("{}", graph);
-        match graph.queue.pop() {
+        match graph1.queue.pop() {
             Some(step) => {
-                graph.complete(&step);
-                graph.queue_completed_steps();
-                order.push(step);
-            },
+                graph1.complete(&step);
+                graph1.queue_completed_steps(&vec![]);
+                order1.push(step);
+            }
             None => break,
         }
     }
-    let answer1 = String::from_iter(order);
-    let answer2 = 0;
+    let answer1 = String::from_iter(order1);
+
+    let mut graph2 = graph.clone();
+    let mut workers = vec![0, 0, 0, 0, 0];
+    let mut tasks: Vec<Option<char>> = vec![None, None, None, None, None];
+    let mut counter = 0;
+    loop {
+        graph2.queue_completed_steps(&tasks);
+
+        for worker in 0..5 {
+            if workers[worker] == 0 && tasks[worker].is_some() {
+                graph2.complete(&tasks[worker].unwrap());
+                graph2.queue_completed_steps(&tasks);
+                tasks[worker] = None;
+            }
+        }
+
+        for worker in 0..5 {
+            if workers[worker] == 0 && tasks[worker].is_none() {
+                if let Some(next_task) = graph2.queue.pop() {
+                    tasks[worker] = Some(next_task);
+                    workers[worker] = secs(next_task)
+                }
+            }
+        }
+
+        for worker in 0..5 {
+            if workers[worker] != 0 {
+                workers[worker] -= 1;
+            }
+        }
+
+        println!("{:?}", workers);
+        println!("{:?}", tasks);
+        println!("{}", counter);
+        if graph2.queue.is_empty() && all(&tasks, |&task| task.is_none()) {
+            break;
+        } else {
+            counter += 1;
+        }
+    }
+    let answer2 = counter;
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -92,20 +141,23 @@ mod tests {
     #[test]
     fn check_day07_case01() {
         full_test(
-"Step C must be finished before step A can begin.
+            "Step C must be finished before step A can begin.
 Step C must be finished before step F can begin.
 Step A must be finished before step B can begin.
 Step A must be finished before step D can begin.
 Step B must be finished before step E can begin.
 Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.", // INPUT STRING
-"CABDFE", // PART 1 RESULT
-"0" // PART 2 RESULT
+            "CABDFE", // PART 1 RESULT
+            "0",      // PART 2 RESULT
         )
     }
 
     fn full_test(input_text: &str, part1_result: &str, part2_result: &str) {
         let input_lines = load_input(input_text);
-        assert_eq!(day07(&input_lines), (part1_result.to_string(), part2_result.to_string()));
+        assert_eq!(
+            day07(&input_lines),
+            (part1_result.to_string(), part2_result.to_string())
+        );
     }
 }
