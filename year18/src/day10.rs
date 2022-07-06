e585f9b8aa9ca07c2846efc404986672a50f5b56
Alexander Tometzki <alex.tometzki@icloud.com>
2022-07-06 21:42:18 +0100

    Start using pixels lib

--- year18/src/day10.rs
+++ year18/src/day10.rs
@@ -1,12 +1,23 @@
 // Potential improvements:
 //
+use log::error;
+use pixels::{Pixels, SurfaceTexture};
 use regex::Regex;
 use std::fmt;
 use std::num::ParseIntError;
 use std::ops::AddAssign;
 use std::str::FromStr;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
 
-#[derive(Clone, Copy)]
+
+const WIDTH: u32 = 640;
+const HEIGHT: u32 = 480;
+
+#[derive(Clone, Copy, PartialEq)]
 struct Vec2d {
     x: i32,
     y: i32,
@@ -75,24 +86,34 @@ impl FromStr for Star {
 
 struct NightSky {
     stars: Vec<Star>,
-    northwest: Vec2d,
-    southeast: Vec2d,
-    time: i32,
+    time: u32,
 }
 
 impl NightSky {
-    fn new(stars: Vec<Star>, northwest: Vec2d, southeast: Vec2d) -> NightSky {
-        NightSky {
-            stars,
-            northwest,
-            southeast,
-            time: 0,
-        }
+    fn new(stars: Vec<Star>) -> NightSky {
+        NightSky { stars, time: 0 }
     }
 
     fn update(&mut self) {
         self.stars.iter_mut().for_each(|star| star.shoot());
         self.time += 1;
+    }
+
+    fn draw(&self, frame: &mut [u8]) {
+        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let x = i as i32 % WIDTH as i32;
+            let y = i as i32 / WIDTH as i32;
+
+            let p = Vec2d::new(x, y);
+
+            let rgba = if self.stars.iter().any(|star| star.p == p) {
+                [0xff, 0xff, 0xff, 0xff]
+            } else {
+                [0x00, 0x00, 0x5a, 0xff]
+            };
+
+            pixel.copy_from_slice(&rgba);
+        }
     }
 }
 
@@ -101,32 +122,79 @@ pub fn day10(input_lines: &[Vec<String>]) -> (String, String) {
         .iter()
         .map(|line| line.parse::<Star>().unwrap())
         .collect();
-    let north: i32 = stars
-        .iter()
-        .max_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
-        .map(|star| star.p.y)
-        .unwrap();
-    let south: i32 = stars
-        .iter()
-        .min_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
-        .map(|star| star.p.y)
-        .unwrap();
-    let east: i32 = stars
-        .iter()
-        .min_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
-        .map(|star| star.p.x)
-        .unwrap();
-    let west: i32 = stars
-        .iter()
-        .max_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
-        .map(|star| star.p.x)
-        .unwrap();
+    // let north: i32 = stars
+    //     .iter()
+    //     .max_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
+    //     .map(|star| star.p.y)
+    //     .unwrap();
+    // let south: i32 = stars
+    //     .iter()
+    //     .min_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
+    //     .map(|star| star.p.y)
+    //     .unwrap();
+    // let east: i32 = stars
+    //     .iter()
+    //     .min_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
+    //     .map(|star| star.p.x)
+    //     .unwrap();
+    // let west: i32 = stars
+    //     .iter()
+    //     .max_by(|star1, star2| star1.p.y.cmp(&star2.p.y))
+    //     .map(|star| star.p.x)
+    //     .unwrap();
 
-    let mut sky = NightSky::new(stars, Vec2d::new(west, north), Vec2d::new(east, south));
+    let mut sky = NightSky::new(stars);
 
-    while sky.time <= 100 {
-        sky.update();
-    }
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Day 10")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture).unwrap()
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        if let Event::RedrawRequested(_) = event {
+            sky.draw(pixels.get_frame());
+            if pixels
+                .render()
+                .map_err(|e| error!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+
+            // Update internal state and request a redraw
+            sky.update();
+            window.request_redraw();
+        }
+    });
 
     let answer1 = 0;
     let answer2 = 0;
@@ -141,7 +209,37 @@ mod tests {
     #[test]
     fn check_day10_case01() {
         full_test(
-            "",  // INPUT STRING
+            "position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>", // INPUT STRING
             "0", // PART 1 RESULT
             "0", // PART 2 RESULT
         )
