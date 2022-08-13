65570c266044fcd5bbcbb716422000894260660d
Alexander Tometzki <alex.tometzki@icloud.com>
2022-08-13 01:10:07 +0100

    some oop

--- year18/src/day12.rs
+++ year18/src/day12.rs
@@ -2,10 +2,24 @@
 //
 use std::str::{FromStr, ParseBoolError};
 
-#[derive(Debug)]
+use itertools::Itertools;
+
+const RULE_SIZE: usize = 5;
+
+#[derive(Clone, Debug)]
 struct SpreadingRule {
     slice: Vec<bool>,
     output: bool,
+}
+
+impl SpreadingRule {
+    fn slice_to_output(&self, other_slice: &[bool]) -> Option<bool> {
+        if &self.slice == other_slice {
+            Some(self.output)
+        } else {
+            None
+        }
+    }
 }
 
 impl FromStr for SpreadingRule {
@@ -14,29 +28,74 @@ impl FromStr for SpreadingRule {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let parts = s.split(" => ").collect::<Vec<&str>>();
         Ok(SpreadingRule {
-            slice: parts[0].chars().map(|c| c == '#').collect(),
+            slice: parts[0].chars().map(|c| c == '#').collect_vec(),
             output: parts[1] == "#",
         })
     }
 }
 
-fn parse(input_lines: &[Vec<String>]) -> (Vec<bool>, Vec<SpreadingRule>) {
-    let gen_zero: Vec<bool> = input_lines[0][0]
-        .split("initial state: ")
-        .collect::<Vec<&str>>()[1]
-        .chars()
-        .map(|c| c == '#')
-        .collect();
+struct Generation {
+    plants: Vec<bool>,
+}
 
+impl FromStr for Generation {
+    type Err = ParseBoolError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let plants: Vec<bool> = s
+            .split("initial state: ")
+            .collect::<Vec<&str>>()[1]
+            .chars()
+            .map(|c| c == '#')
+            .collect();
+        Ok(Generation { plants })
+    }
+}
+
+impl Generation {
+    fn next_generation(&self, rules: Vec<SpreadingRule>) -> Generation {
+        let next_gen_plants = self.plants
+            .clone()
+            .windows(RULE_SIZE)
+            .map(|window| {
+                let mut output: bool = window[2];
+                for rule in &rules {
+                    if let Some(value) = rule.slice_to_output(window) {
+                        output = value;
+                        break
+                    }
+                }
+                output
+            }).collect_vec();
+        Generation { plants: next_gen_plants }
+    }
+}
+
+impl std::fmt::Display for Generation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let out = self.plants.iter().fold(String::new(), |s, &b| {
+            if b {
+                format!("{s}#")
+            } else {
+                format!("{s}.")
+            }
+        });
+        write!(f, "{}", out)
+    }
+}
+
+
+pub fn day12(input_lines: &[Vec<String>]) -> (String, String) {
+    let gen_zero: Generation = input_lines[0][0].parse::<Generation>().expect("Could not parse first line of input into `Generation`.");
     let rules: Vec<SpreadingRule> = input_lines[0][1..]
         .iter()
         .map(|rule| rule.parse::<SpreadingRule>().unwrap())
         .collect();
-    (gen_zero, rules)
-}
+    
+    let gen_one = gen_zero.next_generation(rules);
 
-pub fn day12(input_lines: &[Vec<String>]) -> (String, String) {
-    let (gen_zero, rules) = parse(input_lines);
+    println!("{}", gen_zero);
+    println!("{}", gen_one);
 
     let answer1 = 0;
     let answer2 = 0;
@@ -51,8 +110,23 @@ mod tests {
     #[test]
     fn check_day12_case01() {
         full_test(
-            "",  // INPUT STRING
-            "0", // PART 1 RESULT
+            "initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #",  // INPUT STRING
+            "1", // PART 1 RESULT
             "0", // PART 2 RESULT
         )
     }
