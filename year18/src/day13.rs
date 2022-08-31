74789f782aec1d45a183187aa26bf3eb264f7852
Alex Tometzki <alex.tometzki@metaswitch.com>
2022-08-31 15:08:20 +0100

    Extract all the carts from the start

--- year18/src/day13.rs
+++ year18/src/day13.rs
@@ -2,10 +2,10 @@ use itertools::Itertools;
 use nalgebra::{Vector2, Matrix2, DMatrix, RowDVector};
 use rotate_enum::RotateEnum;
 
-// const UP: Vector2<i32> = Vector2::new(0, 1);
-// const DOWN: Vector2<i32> = Vector2::new(0, -1);
-// const LEFT: Vector2<i32> = Vector2::new(-1, 0);
-// const RIGHT: Vector2<i32> = Vector2::new(1, 0);
+const UP: Vector2<i32> = Vector2::new(0, 1);
+const DOWN: Vector2<i32> = Vector2::new(0, -1);
+const LEFT: Vector2<i32> = Vector2::new(-1, 0);
+const RIGHT: Vector2<i32> = Vector2::new(1, 0);
 const TURN_LEFT: Matrix2<i32> = Matrix2::new(0, 1, -1, 0);
 const GO_STRAIGHT: Matrix2<i32> = Matrix2::new(1, 0, 0, 1);
 const TURN_RIGHT: Matrix2<i32> = Matrix2::new(0, -1, 1, 0);
@@ -15,6 +15,17 @@ enum IntersectionBehavior {
     TurnLeft,
     GoStraight,
     TurnRight,
+}
+
+impl std::fmt::Display for IntersectionBehavior {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let c = match self {
+            IntersectionBehavior::TurnLeft => 'L',
+            IntersectionBehavior::GoStraight => '|',
+            IntersectionBehavior::TurnRight => 'R',
+        };
+        write!(f, "{}", c)
+    }
 }
 
 impl Into<Matrix2<i32>> for IntersectionBehavior {
@@ -33,10 +44,32 @@ struct Cart {
     intersection_behavior: IntersectionBehavior,
 }
 
+impl std::fmt::Display for Cart {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let arrow = match (self.velocity.x, self.velocity.y) {
+            (1, 0) => '>',
+            (0, 1) => '^',
+            (-1, 0) => '<',
+            (0, -1) => 'v',
+            _ => panic!(),
+        };
+        write!(f, "{} {} : ({}, {})", arrow, self.intersection_behavior, self.position.x, self.position.y)
+    }
+}
+
 impl Cart {
+    fn new(x: i32, y: i32, velocity: Vector2<i32>) -> Self {
+        Self {
+            position: Vector2::new(x, y),
+            velocity, 
+            intersection_behavior: IntersectionBehavior::TurnLeft,
+        }
+    }
+
     fn go(&mut self, track_map: DMatrix<char>) {
         self.position += self.velocity;
-        // let track_piece: char = track_map[Into::<(i32, i32)>::into(self.position)]; 
+        let track_piece: char = track_map[(self.position.y as usize, self.position.x as usize)];
+        self.handle_track_piece(track_piece);
     }
 
     fn handle_track_piece(&mut self, track_piece: char) {
@@ -76,14 +109,41 @@ impl Cart {
 
 pub fn day13(input_lines: &[Vec<String>]) -> (String, String) {
     
-    let mut track_map: DMatrix<char> = DMatrix::from_rows(
+    let track_map: DMatrix<char> = DMatrix::from_rows(
         &input_lines[0]
             .iter()
             .map(|line| RowDVector::from_vec(line.chars().collect_vec()))
             .collect_vec()
     );
-    
+    // Add padding of one row around the edge so that we can take 3x3 slices and see the full surroundings of each cart.
+    let nrows = track_map.nrows();
+    let ncols = track_map.ncols();
+    let track_map = track_map.insert_row(nrows, ' ');
+    let track_map = track_map.insert_column( ncols, ' ');
+    let track_map = track_map.insert_row(0, ' ');
+    let mut track_map = track_map.insert_column(0, ' ');
 
+    // Find all the carts
+    let cart_surrounding_size = 3;
+    let mut carts: Vec<Cart> = Vec::new();
+    for jj in 0..track_map.nrows()-cart_surrounding_size {
+        for ii in 0..track_map.ncols()-cart_surrounding_size {
+            let slice = track_map.slice_mut((jj, ii), (cart_surrounding_size, cart_surrounding_size));
+            match slice[(1, 1)] {
+                '>' => carts.push(Cart::new(ii as i32, jj as i32, RIGHT)),
+                '<' => carts.push(Cart::new(ii as i32, jj as i32, LEFT)),
+                '^' => carts.push(Cart::new(ii as i32, jj as i32, UP)),
+                'v' => carts.push(Cart::new(ii as i32, jj as i32, DOWN)),
+                _ => continue,
+            }
+        }
+    }
+
+    for cart in carts {
+        println!("{}", cart);
+    }
+
+    
     let answer1 = 0;
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
