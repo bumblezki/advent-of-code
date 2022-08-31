14edf637d4e71284a7806231cebc3539c008f46a
Alex Tometzki <alex.tometzki@metaswitch.com>
2022-08-31 12:25:29 +0100

    first attempt at day13

--- year18/src/day13.rs
+++ year18/src/day13.rs
@@ -1,5 +1,104 @@
-// Potential improvements:
-//
+use nalgebra::{Vector2, Matrix2, DMatrix};
+use rotate_enum::RotateEnum;
+
+const UP: Vector2<i32> = Vector2::new(0, 1);
+const DOWN: Vector2<i32> = Vector2::new(0, -1);
+const LEFT: Vector2<i32> = Vector2::new(-1, 0);
+const RIGHT: Vector2<i32> = Vector2::new(1, 0);
+const TURN_LEFT: Matrix2<i32> = Matrix2::new(0, 1, -1, 0);
+const GO_STRAIGHT: Matrix2<i32> = Matrix2::new(1, 0, 0, 1);
+const TURN_RIGHT: Matrix2<i32> = Matrix2::new(0, -1, 1, 0);
+
+#[derive(Debug, Clone, Copy, RotateEnum)]
+enum IntersectionBehaviour {
+    TurnLeft,
+    GoStraight,
+    TurnRight,
+}
+
+impl Into<Matrix2<i32>> for IntersectionBehaviour {
+    fn into(self) -> Matrix2<i32> {
+        match self {
+            IntersectionBehaviour::TurnLeft => TURN_LEFT,
+            IntersectionBehaviour::GoStraight => GO_STRAIGHT,
+            IntersectionBehaviour::TurnRight => TURN_RIGHT,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Velocity {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Into<Vector2<i32>> for Velocity {
+    fn into(self) -> Vector2<i32> {
+        match self {
+            Velocity::Up => UP,
+            Velocity::Down => DOWN,
+            Velocity::Left => LEFT,
+            Velocity::Right => RIGHT,
+        }
+    }
+}
+
+impl From<Vector2<i32>> for Velocity {
+    fn from(value: Vector2<i32>) -> Self {
+        match value {
+            UP => Velocity::Up,
+            DOWN => Velocity::Down,
+            LEFT => Velocity::Left,
+            RIGHT => Velocity::Right,
+            _ => panic!(),
+        }
+    }
+}
+
+struct Cart {
+    position: Vector2<i32>,
+    velocity: Velocity,
+    intersection_behaviour: IntersectionBehaviour,
+}
+
+impl Cart {
+    fn go(&mut self) {
+        self.position += Into::<Vector2<i32>>::into(self.velocity);
+    }
+
+    fn handle_intersection(&mut self) {
+        self.velocity = Velocity::from(Into::<Matrix2<i32>>::into(self.intersection_behaviour) * Into::<Vector2<i32>>::into(self.velocity));
+        self.intersection_behaviour = self.intersection_behaviour.next();
+    }
+
+    // fn handle_turn()
+
+        
+    //         '\\' => {
+    //             match self.velocity {
+    //                 Velocity::Up | Velocity::Down => TURN_LEFT,
+    //                 Velocity::Right | Velocity::Left => TURN_RIGHT,
+    //             }
+    //         },
+    //         '/' => {
+    //             match self.velocity {
+    //                 Velocity::Up | Velocity::Down => TURN_RIGHT,
+    //                 Velocity::Left | Velocity::Right => TURN_LEFT,
+    //             }
+    //         },
+    //         '-' | '|' => {
+    //             GO_STRAIGHT
+    //         }
+    //         _ => {
+    //             panic!()
+    //         }
+    //     }
+    // 
+}
+
+
 
 pub fn day13(_input_lines: &[Vec<String>]) -> (String, String) {
     let answer1 = 0;
