d662150ac37f5924925e4174e831a270fa19b747
Alex Tometzki <alex.tometzki@metaswitch.com>
2022-10-19 14:47:21 +0100

    solve y20-d2 but can I make it better?

--- year20/src/day02.rs
+++ year20/src/day02.rs
@@ -1,9 +1,82 @@
-// Potential improvements:
-//
+#![deny(clippy::pedantic)]
 
-pub fn day02(_input_lines: &[Vec<String>]) -> (String, String) {
-    let answer1 = 0;
-    let answer2 = 0;
+use std::str::FromStr;
+use std::convert::Infallible;
+use regex::Regex;
+use counter::Counter;
+
+struct PasswordPolicy {
+    required_char: char,
+    d1: usize,
+    d2: usize,
+}
+
+struct PasswordChecker {
+    pw: String, 
+    policy: PasswordPolicy,
+}
+
+impl FromStr for PasswordChecker {
+    type Err = Infallible;
+    
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"(\d+)-(\d+) ([a-z]): (\w+)").unwrap();
+        re.captures(s)
+            .map(|cap| {
+                let d1: usize = FromStr::from_str(&cap[1]).unwrap();
+                let d2: usize = FromStr::from_str(&cap[2]).unwrap();
+                let required_char: char = FromStr::from_str(&cap[3]).unwrap();
+                let pw: String = FromStr::from_str(&cap[4]).unwrap();
+                Ok(
+                    PasswordChecker::new(
+                        pw,
+                        required_char,
+                        d1,
+                        d2,
+                    )
+                )
+            })
+            .expect("Failed to match regular expression against input.")
+    }
+}
+
+impl PasswordChecker {
+    fn new(pw: String, required_char: char, d1: usize, d2: usize) -> Self {
+        Self {
+            pw,
+            policy: PasswordPolicy {
+                required_char,
+                d1,
+                d2,
+            }
+        }
+    }
+
+    fn validate_occurance_count(&self) -> bool {
+        let pw_chars_counter = &self.pw.chars().collect::<Counter<char>>();
+        self.policy.d1 <= pw_chars_counter[&self.policy.required_char] && pw_chars_counter[&self.policy.required_char] <= self.policy.d2 
+    }
+
+    fn validate_occurance_positions(&self) -> bool {
+        let pw_chars: Vec<char> = self.pw.chars().collect();
+        (pw_chars[self.policy.d1-1] == self.policy.required_char && pw_chars[self.policy.d2-1] != self.policy.required_char) ||
+        (pw_chars[self.policy.d1-1] != self.policy.required_char && pw_chars[self.policy.d2-1] == self.policy.required_char)
+    }
+}
+
+
+
+pub fn day02(input_lines: &[Vec<String>]) -> (String, String) {
+    let (answer1, answer2) = input_lines[0].iter().fold((0, 0), |(mut acc1, mut acc2), line| {
+        let checker: PasswordChecker = line.parse().unwrap();
+        if checker.validate_occurance_count() {
+            acc1 += 1;
+        };
+        if checker.validate_occurance_positions() {
+            acc2 += 1;
+        }
+        (acc1, acc2)
+    });
     (format!("{}", answer1), format!("{}", answer2))
 }
 
