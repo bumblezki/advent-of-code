908367790204c36eab7ef5c7e7181f693aae28b8
Alex Tometzki <alex.tometzki@metaswitch.com>
2022-10-25 11:01:25 +0100

    Add comments

--- year20/src/day05.rs
+++ year20/src/day05.rs
@@ -1,21 +1,25 @@
 fn str_to_decimal(s: &str, one_char: char, zero_char: char) -> u64 {
-    u64::from_str_radix(
-        &s.chars()
-            .map(|c| match c {
-                one if one == one_char => '1',
-                zero if zero == zero_char => '0',
-                _ => panic!(
-                    "Character in seat address was not {} or {}",
-                    one_char, zero_char
-                ),
-            })
-            .collect::<String>(),
-        2,
-    )
-    .unwrap()
+    // Map the string slice to a binary number as a String.
+    let binary_string = s
+        .chars()
+        .map(|c| match c {
+            one if one == one_char => '1',
+            zero if zero == zero_char => '0',
+            _ => panic!(
+                "Character in seat address was not {} or {}",
+                one_char, zero_char
+            ),
+        })
+        .collect::<String>();
+    // Then convert this binary string to an integer.
+    u64::from_str_radix(&binary_string, 2).unwrap()
 }
 
 pub fn day05(input_lines: &[Vec<String>]) -> (String, String) {
+    // The seat address can be converted to a row and column by splitting it into it's two parts, converting
+    // those parts into a binary string  and then converting those binary numbers into integers. Then we can
+    // use the row and column to find the seat ID with row * 8 + col.
+    // E.g., "FBFBBFFRLR" -> "FBFBBFF", "RLR" -> "0101100", "101" -> 44, 5 -> 44 * 8 + 5 = 357
     let mut seat_ids: Vec<u64> = input_lines[0]
         .iter()
         .map(|line| {
@@ -32,12 +36,15 @@ pub fn day05(input_lines: &[Vec<String>]) -> (String, String) {
 
     let mut answer2: u64 = 0;
     for id_window in seat_ids.windows(2) {
+        // Calling vec![1, 2, 3, 5, 6, 7].windows(2) yields &[1, 2] then &[2, 3] then &[3, 5], etc.
+        // If the difference between adjacent seat IDs in the sorted vec is greater than 1 then we
+        // have found our seat.
         if id_window[0] + 1 < id_window[1] {
             answer2 = id_window[0] + 1;
         }
     }
 
-    (format!("{:?}", answer1), format!("{}", answer2))
+    (format!("{}", answer1), format!("{}", answer2))
 }
 
 #[cfg(test)]
