cef5447afb6e025ca0aff03187b5a1f8d71ea85a
Alex Tometzki <alexto@microsoft.com>
2023-12-12 13:15:50 +0000

    solve day03a

--- year23/src/day03.rs
+++ year23/src/day03.rs
@@ -1,5 +1,5 @@
-// Potential improvements:
-//
+use std::collections::HashMap;
+
 fn clamp<T: PartialOrd>(input: T, min: T, max: T) -> T {
     if input < min {
         min
@@ -11,54 +11,47 @@ fn clamp<T: PartialOrd>(input: T, min: T, max: T) -> T {
 }
 
 pub fn day03(input_lines: &[Vec<String>]) -> (String, String) {
-    let num_rows = input_lines[0].len();
-    let num_cols = input_lines[0][0].len();
+
+    let height = input_lines[0].len();
+
+    let mut gear_map = HashMap::new();
 
     let mut total: u32 = 0;
 
     for (ii, row) in input_lines[0].iter().enumerate() {
-        let mut current_num_str = Vec::new();
-        let mut jj_chars = row.chars().enumerate();
+
+        let mut num_builder = Vec::new();
         let mut start_col: usize = 0;
-        while let Some((jj, cc)) = jj_chars.next() {
+        for (jj, cc) in row.char_indices() {
             if cc.is_ascii_digit() {
-                if current_num_str.is_empty() {
+                if num_builder.is_empty() {
                     start_col = jj;
                 }
-                current_num_str.push(cc);
-            } else if !current_num_str.is_empty() {
-                let current_num = current_num_str
-                    .iter()
-                    .collect::<String>()
-                    .parse::<u32>()
-                    .unwrap();
-                current_num_str.clear();
+                num_builder.push(cc);
+            }
 
-                'search: for yy in ii.saturating_sub(1)..=ii + 1 {
-                    for xx in start_col.saturating_sub(1)..=jj {
-                        println!(
-                            "input_lines: {:?}",
-                            input_lines[clamp(yy, 0, num_rows - 1)][clamp(xx, 0, num_cols - 1)]
-                        );
-                        let neighbour = input_lines[clamp(yy, 0, num_rows - 1)]
-                            [clamp(xx, 0, num_cols - 1)]
-                        .chars()
-                        .next()
-                        .unwrap();
-                        if !neighbour.is_ascii_digit() && neighbour != '.' {
-                            total += current_num;
-                            println!("{}: {}", current_num, neighbour);
-                            break 'search;
-                        }
+            if !num_builder.is_empty() && (!cc.is_ascii_digit() || jj == row.len() - 1) {
+                let num = num_builder.iter().collect::<String>().parse::<u32>().unwrap();
+                num_builder.clear();
+                let mut touches = false;
+                for yy in clamp(ii.saturating_sub(1), 0, height - 1)..=clamp(ii+1, 0, height - 1) {
+                    for xx in clamp(start_col.saturating_sub(1), 0, row.len() - 1)..clamp(jj+1, 0, row.len() - 1) {
+                        gear_map.insert((xx, yy), num);
+                        let this_char = input_lines[0][yy].chars().nth(xx).unwrap();
+                        if this_char != '.' && !this_char.is_ascii_digit() {
+                            touches = true;
+                        } 
                     }
                 }
-            
-            }
+                if touches {
+                    total += num;
+                }
+            } 
         }
     }
 
-    let answer1: u32 = total;
-    let answer2 = 10;
+    let answer1 = total;
+    let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -70,7 +63,7 @@ mod tests {
     #[test]
     fn check_day03_case01() {
         full_test(
-            "467..114..
+"467..114..
 ...*......
 ..35..633.
 ......#...
@@ -80,16 +73,13 @@ mod tests {
 ......755.
 ...$.*....
 .664.598..", // INPUT STRING
-            "4361", // PART 1 RESULT
-            "0",    // PART 2 RESULT
+"4361", // PART 1 RESULT
+"0" // PART 2 RESULT
         )
     }
 
     fn full_test(input_text: &str, part1_result: &str, part2_result: &str) {
         let input_lines = load_input(input_text);
-        assert_eq!(
-            day03(&input_lines),
-            (part1_result.to_string(), part2_result.to_string())
-        );
+        assert_eq!(day03(&input_lines), (part1_result.to_string(), part2_result.to_string()));
     }
 }
