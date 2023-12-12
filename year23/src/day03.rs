1a9a45a8ad2dfbb8eab8410b454b7c61cbc22807
Alex Tometzki <alexto@microsoft.com>
2023-12-12 14:00:32 +0000

    fmt day03

--- year23/src/day03.rs
+++ year23/src/day03.rs
@@ -11,7 +11,6 @@ fn clamp<T: PartialOrd>(input: T, min: T, max: T) -> T {
 }
 
 pub fn day03(input_lines: &[Vec<String>]) -> (String, String) {
-
     let height = input_lines[0].len();
 
     let mut gear_map: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
@@ -19,7 +18,6 @@ pub fn day03(input_lines: &[Vec<String>]) -> (String, String) {
     let mut answer1: u32 = 0;
 
     for (ii, row) in input_lines[0].iter().enumerate() {
-
         let mut num_builder = Vec::new();
         let mut start_col: usize = 0;
         for (jj, cc) in row.char_indices() {
@@ -31,38 +29,44 @@ pub fn day03(input_lines: &[Vec<String>]) -> (String, String) {
             }
 
             if !num_builder.is_empty() && (!cc.is_ascii_digit() || jj == row.len() - 1) {
-                let num = num_builder.iter().collect::<String>().parse::<u32>().unwrap();
+                let num = num_builder
+                    .iter()
+                    .collect::<String>()
+                    .parse::<u32>()
+                    .unwrap();
                 num_builder.clear();
                 let mut touches = false;
-                for yy in clamp(ii.saturating_sub(1), 0, height - 1)..=clamp(ii+1, 0, height - 1) {
-                    for xx in clamp(start_col.saturating_sub(1), 0, row.len() - 1)..clamp(jj+1, 0, row.len() - 1) {
+                for yy in clamp(ii.saturating_sub(1), 0, height - 1)..=clamp(ii + 1, 0, height - 1)
+                {
+                    for xx in clamp(start_col.saturating_sub(1), 0, row.len() - 1)
+                        ..clamp(jj + 1, 0, row.len() - 1)
+                    {
                         gear_map.entry((xx, yy)).or_default().push(num);
                         let this_char = input_lines[0][yy].chars().nth(xx).unwrap();
                         if this_char != '.' && !this_char.is_ascii_digit() {
                             touches = true;
-                        } 
+                        }
                     }
                 }
                 if touches {
                     answer1 += num;
                 }
-            } 
-        }
-    }
-    
-    let mut answer2 = 0;
-    for (ii, row) in input_lines[0].iter().enumerate() {
-        for (jj, cc) in row.char_indices() {
-            if cc == '*' {
-                if let Some(nums) = gear_map.get(&(jj, ii)) {
-                    if nums.len() == 2 {
-                        answer2 += nums[0] * nums[1];
-                    }
-                }
             }
         }
     }
-    
+
+    let mut answer2 = 0;
+    for (ii, jj) in input_lines[0].iter().enumerate().flat_map(|(ii, row)| {
+        row.char_indices()
+            .filter_map(move |(jj, cc)| if cc == '*' { Some((ii, jj)) } else { None })
+    }) {
+        if let Some(nums) = gear_map.get(&(jj, ii)) {
+            if nums.len() == 2 {
+                answer2 += nums[0] * nums[1];
+            }
+        }
+    }
+
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -74,7 +78,7 @@ mod tests {
     #[test]
     fn check_day03_case01() {
         full_test(
-"467..114..
+            "467..114..
 ...*......
 ..35..633.
 ......#...
@@ -84,13 +88,16 @@ mod tests {
 ......755.
 ...$.*....
 .664.598..", // INPUT STRING
-"4361", // PART 1 RESULT
-"467835" // PART 2 RESULT
+            "4361",   // PART 1 RESULT
+            "467835", // PART 2 RESULT
         )
     }
 
     fn full_test(input_text: &str, part1_result: &str, part2_result: &str) {
         let input_lines = load_input(input_text);
-        assert_eq!(day03(&input_lines), (part1_result.to_string(), part2_result.to_string()));
+        assert_eq!(
+            day03(&input_lines),
+            (part1_result.to_string(), part2_result.to_string())
+        );
     }
 }
